d744811e168d7382fc33663f2cc7c66742d2c500
Cristina Sanz <cristina.sanz508@comunidadunir.net>
2025-03-02 17:22:12 +0100

    UPDATE: JavaDoc en Clase Empleado.java

--- src/main/java/Empleado.java
+++ src/main/java/Empleado.java
@@ -1,65 +1,66 @@
 
 import java.util.Objects;
 
+/**
+ * Clase que permite ver y modificar el salario de un empleado.
+ * @author Cristina Sanz Alonso
+ * @version 1.0
+ */
+
+
 public class Empleado {
 
     private String nombre;
     private double salario;
+
+    /**
+     * Constructor con los paramentros de la clase
+     * @param nombre Es el nombre del empleado
+     * @param salario Es el salario del empleado
+     */
 
     public Empleado(String nombre, double salario) {
         this.nombre = nombre;
         this.salario = salario;
     }
 
+    /**
+     * Constructor por defecto que no recibe valores.
+     */
+
     public Empleado() {
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public double getSalario() {
-        return salario;
-    }
-
-    public void setSalario(double salario) {
-        this.salario = salario;
-    }
-
-    @Override
-    public String toString() {
-        return "Empleado{" +
-                "nombre='" + nombre + '\'' +
-                ", salario=" + salario +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        Empleado empleado = (Empleado) o;
-        return Objects.equals(nombre, empleado.nombre);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(nombre);
+        super();
     }
 
     //Métodos propios:
+
+    /**
+     * Metodo que pide dos parametros:
+     * @param nombre Es el nombre del empleado del tipo String
+     * @param salario Es el salario del empleado de tipo double
+     * @return true
+     */
 
     public boolean empleado(String nombre, double salario) {
         System.out.println("Mi nombre es:" + nombre + " y gano: " + salario);
         return true;
     }
 
+    /**
+     * Metodo que calcula el aumento del salario mediante la suma del porcentaje dado en la entrada
+     * @param porcentaje Es el porcentaje de subida que se aplica
+     * @return El salario con el aumento aplicado
+     */
+
     public double aumentarSalario(double porcentaje) {
-        return salario += porcentaje;
+        return salario + (salario * porcentaje/100);
     }
+
+    /**
+     * Metodo que imprime por el salario del empleado
+     * @param salario Es el salario introducido
+     * @return salario
+     */
 
     public double obtenerSalario(double salario) {
         return salario;
